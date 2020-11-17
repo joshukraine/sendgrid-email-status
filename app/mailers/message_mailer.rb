9a42112d2d753ad5ef50ce864c3d293a95608d0d
Joshua Steele <joshukraine@gmail.com>
2020-11-17 18:55:11 +0200

    --wip--

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,26 @@
 class MessageMailer < ApplicationMailer
   def email
     @message = params[:message]
+    # mail(
+    #   to: @message.to,
+    #   subject: @message.subject,
+    #   from: ENV["FROM_EMAIL"],
+    #   custom_args: {
+    #     id: @message.id
+    #   }
+    # )
+
     mail(
       to: @message.to,
-      subject: @message.subject,
-      from: ENV['FROM_EMAIL'],
+      from: ENV["FROM_EMAIL"],
       custom_args: {
         id: @message.id
-      }
+      },
+      dynamic_template_data: {
+        subject: @message.subject,
+        message_body: @message.body
+      },
+      template_id: ENV["TEMPLATE_ID"]
     )
   end
 end
